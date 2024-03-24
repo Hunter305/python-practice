b3fefc4d4e2de804215ecb39548ac20192bf1639
Hunter305 <avinash8169@gmail.com>
2024-03-24 13:10:37 +0530

    __name__ and "__main__"

--- classes/sample.py
+++ classes/sample.py
@@ -1,3 +1,4 @@
+
 class Dog():
     def __init__(self, name, breed):
         self.name = name
@@ -12,3 +13,20 @@ print(sn.breed)
 sn.breed = "Lab"
 
 print(sn.breed)
+
+
+class Animal():
+    def __init__(self, animal_name):
+        self.animal_name = animal_name
+
+
+class Cat(Animal):
+    def __init__(self, animal_name, name, breed):
+        super().__init__(animal_name)
+        self.name = name
+        self.breed = breed
+
+
+cat1 = Cat(animal_name="cat", name="peter", breed="persian")
+
+print(cat1.animal_name)
---
+++ one.py
@@ -0,0 +1,8 @@
+def func():
+    if __name__ == "__main__":
+        print("this is first level")
+    else:
+        print("this is second level")
+
+
+func()
